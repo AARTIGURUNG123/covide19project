eef4e438256e96bd4de995b38fd9b554ac77fe5e
AARTIGURUNG123 <aartigurung67@gmail.com>
2025-04-10 10:41:10 +0530

    Add files via upload

---
+++ pythonerrorprogram.py
@@ -0,0 +1,21 @@
+value1=2
+value2=5
+for x in range(value1):
+    if x==value2:
+        print("EQUL")
+    else:
+        print("NOTEUL")
+
+
+#Write the following code using the while loop 
+'''for value in range(10,50,5):
+           value*=10
+           print(value)'''
+
+value = 10
+while value < 50:
+    value *= 10
+    print(value)
+    value+=5
+    
+    
